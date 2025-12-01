6cd9887b631ca69fa1608e601e0a985d085ad26b
Karolis <kiaune.karolis@gmail.com>
2025-12-01 14:24:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2024
+
+### How to:
+
+1. cd to a specific day
+2. run command `node solution.js`
+
+### Days track:
---
+++ template/solution.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync("example.txt", "utf-8");
+input = input.split(/\r?\n/);
+
+const firstSolution = () => {};
+
+console.log("first part result:", firstSolution());
+
+const secondSolution = () => {};
+
+console.log("second part result:", secondSolution());
